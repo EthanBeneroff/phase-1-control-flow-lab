aff9a7fca39e188ecc51dbd2435330ca36514724
Ethan Beneroff <ethanbeneroff@gmail.com>
2023-04-09 18:46:43 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(height){
   // Write your code here!
+  let result;
+  if (height > 2500) {result = `No can do.`}
+  else if (height <= 0) {result = `No can do.`}
+  else if (height <= 400){result = `This one is on me!`}
+  else if (height > 2000){result = `I will gladly take your thirty bucks.`}
+  else {result = `That will be twenty bucks.`}
+  return result;
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let result;
+  city === `NYC` ? (result = `Ok, sounds good.`) : (result = `No go.`)
+  return result;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let result;
+  switch (tip){
+    case `generous`: 
+      result = `Thank you so much.`;
+      break;
+    case `not as generous`: 
+      result = `Thank you.`;
+      break;
+    default:
+      result= `Bye.`
+      break;
+    
+
+
+
+
+
+  }
+  return result;
 }
